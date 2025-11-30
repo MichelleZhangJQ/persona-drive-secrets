49be0b8439569c6b4fddabc1afa8af1e39b6ea2b
MichelleZhang <zhangmichellejq@gmail.com>
2025-11-29 20:32:07 -0500

    modified app/theories/page.tsx

---
+++ app/theories/page.tsx
@@ -0,0 +1,11 @@
+// app/theories/page.tsx
+import React from "react";
+
+export default function TheoriesPage() {
+  return (
+    <div>
+      <h1>Section 4: The Triadic Drive Model</h1>
+      {/* ...rest of your page content */}
+    </div>
+  );
+}
